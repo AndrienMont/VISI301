9f73f303701a24fa441d43d3bded4a487979b841
MaxBuster <92114663+MaxBuster380@users.noreply.github.com>
2021-10-30 10:03:17 +0200

    Add files via upload
    algorithme de labyrinthe fonctionnel

--- labyrinthe.py
+++ labyrinthe.py
@@ -27,63 +27,58 @@ def generateMaze(long, larg):
 	
     casesParcourues = []
     indexCaseActive = 0
-    ajouterCase = True
 
     while (len(casesParcourues) < long*larg):
         #Traiter élément
-        if ajouterCase:
-            casesParcourues.append(caseActive)
-            print(x)
-            print(y)
-            print()
-            mazeData[x][y]["set"] = True
+        casesParcourues.append(caseActive)
+        mazeData[x][y]["set"] = True
+
+        if (len(casesParcourues) == long*larg):
+            break
 
         #Obtenir élément suivant
-        ajouterCase = True
-        if checkMovementPossibility(mazeData, x, y): #Si une case à côté est libre
-            randomIndex = randomizeList([i for i in range(0,4)]) #Mélange l'ordre de vérification des index
-            mouvTrouve = False
-            i = 0
-            rI = randomIndex[0]
-            while mouvTrouve == False: #Cherche un mouvement valide aléatoirement
-                if rI == 0: #Haut
-                    mouvTrouve = checkUpMove(mazeData, x, y)
-                elif rI == 1: #Bas
-                    mouvTrouve = checkDownMove(mazeData, x, y)
-                elif rI == 2: #Gauche
-                    mouvTrouve = checkLeftMove(mazeData, x, y)
-                elif rI == 3: #Droite
-                    mouvTrouve = checkRightMove(mazeData, x, y)
-
-                i = i + 1
-                rI = randomIndex[i]
-
-            #Redéfinie la case active
-            if rI == 0: #Haut
-                mazeRes[x][y]["wallUp"] = False #Enlève le mur entre l'ancienne et nouvelle case
-                caseActive = [x, y-1]
-            elif rI == 1: #Bas
-                mazeRes[x][y+1]["wallUp"] = False #Enlève le mur entre l'ancienne et nouvelle case
-                caseActive = [x, y+1]
-            elif rI == 2: #Gauche
-                mazeRes[x-1][y]["wallLeft"] = False #Enlève le mur entre l'ancienne et nouvelle case
-                caseActive = [x-1, y]
-            elif rI == 3: #Droite
-                mazeRes[x+1][y]["wallLeft"] = False #Enlève le mur entre l'ancienne et nouvelle case
-                caseActive = [x+1, y]
-                
-        else: #Si aucun mouvement n'est possible
-            ajouterCase = False 
-            mazeData[x][y]["done"] = True # La case n'est pas à traiter à la suite
+        while checkMovementPossibility(mazeData, x, y) == False: #Si aucun mouvement n'est possible
+            mazeData[x][y]["done"] = True # La case n'est pas à traiter par la suite
 
             while mazeData[x][y]["done"]:
                 indexCaseActive = indexCaseActive - 1
                 
-                casesParcourues[indexCaseActive]
+                caseActive = casesParcourues[indexCaseActive]
                 x = caseActive[0]
                 y = caseActive[1]
-            
-        
+
+        #Obtient nouvelle case    
+        randomIndex = randomizeList([i for i in range(0,4)]) #Mélange l'ordre de vérification des index
+        mouvTrouve = False
+        i = 0
+        rI = randomIndex[0]
+        while mouvTrouve == False: #Cherche un mouvement valide aléatoirement
+            if rI == 0: #Haut
+                mouvTrouve = checkUpMove(mazeData, x, y)
+            elif rI == 1: #Bas
+                mouvTrouve = checkDownMove(mazeData, x, y)
+            elif rI == 2: #Gauche
+                mouvTrouve = checkLeftMove(mazeData, x, y)
+            elif rI == 3: #Droite
+                mouvTrouve = checkRightMove(mazeData, x, y)
+
+            if mouvTrouve == False:
+                i = i + 1
+                rI = randomIndex[i]
+
+        #Redéfinie la case active
+        if rI == 0: #Haut
+            mazeRes[x][y]["wallUp"] = False #Enlève le mur entre l'ancienne et nouvelle case
+            caseActive = [x, y-1]
+        elif rI == 1: #Bas
+            mazeRes[x][y+1]["wallUp"] = False #Enlève le mur entre l'ancienne et nouvelle case
+            caseActive = [x, y+1]
+        elif rI == 2: #Gauche
+            mazeRes[x][y]["wallLeft"] = False #Enlève le mur entre l'ancienne et nouvelle case
+            caseActive = [x-1, y]
+        elif rI == 3: #Droite
+            mazeRes[x+1][y]["wallLeft"] = False #Enlève le mur entre l'ancienne et nouvelle case
+            caseActive = [x+1, y]
             
 
         indexCaseActive = len(casesParcourues) #La case active est la dernière
@@ -99,26 +94,11 @@ def checkMovementPossibility(mazeDataF1, xF1, yF1):
         x : int | abscisse de la case à vérifier
         y : int | largeur du labyrinthe
         mazeDataF1 : tableau de tableau de cases | labyrinthe
-
     OUT
         boolean
     """
 
-    mouvCounter = 4
-
-    if checkLeftMove(mazeDataF1, xF1, yF1) == False:
-        mouCounter = mouvCounter - 1
-
-    if checkRightMove(mazeDataF1, xF1, yF1) == False:
-        mouCounter = mouvCounter - 1
-
-    if checkUpMove(mazeDataF1, xF1, yF1) == False:
-        mouCounter = mouvCounter - 1
-
-    if checkDownMove(mazeDataF1, xF1, yF1) == False:
-        mouCounter = mouvCounter - 1
-
-    return mouvCounter > 0
+    return checkLeftMove(mazeDataF1, xF1, yF1) or checkRightMove(mazeDataF1, xF1, yF1) or checkUpMove(mazeDataF1, xF1, yF1) or checkDownMove(mazeDataF1, xF1, yF1)
 
 def checkLeftMove(mazeDataF2, xF2, yF2):
     """Vérifie si un mouvement à gauche est possible
@@ -129,16 +109,10 @@ def checkLeftMove(mazeDataF2, xF2, yF2):
         mazeDataF1 : tableau de tableau de cases | labyrinthe
 
     OUT
-        res : boolean
+        boolean
     """
-    res = True
-    if xF2 > 0: 
-        if mazeDataF2[xF2-1][yF2]["set"]: #Si la case de gauche est définie, il y a un mouvement de moins possible
-            res = False
-    else: #Si la case est tout à gauche, il y a un mouvement de moins possible
-        res = False
 
-    return res
+    return xF2 > 0 and mazeDataF2[xF2-1][yF2]["set"] == False
 
 def checkRightMove(mazeDataF2, xF2, yF2):
     """Vérifie si un mouvement à droite est possible
@@ -149,16 +123,9 @@ def checkRightMove(mazeDataF2, xF2, yF2):
         mazeDataF1 : tableau de tableau de cases | labyrinthe
 
     OUT
-        res : boolean
+        boolean
     """
-    res = True
-    if xF2 < len(mazeDataF2): 
-        if mazeDataF2[xF2+1][yF2]["set"]: #Si la case de droite est définie, il y a un mouvement de moins possible
-            res = False
-    else: #Si la case est tout à droite, il y a un mouvement de moins possible
-        res = False
-
-    return res
+    return xF2 < len(mazeDataF2)-1 and mazeDataF2[xF2+1][yF2]["set"] == False
 
 def checkUpMove(mazeDataF2, xF2, yF2):
     """Vérifie si un mouvement en haut est possible
@@ -169,16 +136,10 @@ def checkUpMove(mazeDataF2, xF2, yF2):
         mazeDataF1 : tableau de tableau de cases | labyrinthe
 
     OUT
-        res : boolean
+        boolean
     """
-    res = True
-    if yF2 > 0: 
-        if mazeDataF2[xF2][yF2-1]["set"]: #Si la case du haut est définie, il y a un mouvement de moins possible
-            res = False
-    else: #Si la case est tout en haut, il y a un mouvement de moins possible
-        res = False
 
-    return res
+    return yF2 > 0 and mazeDataF2[xF2][yF2-1]["set"] == False
 
 def checkDownMove(mazeDataF2, xF2, yF2):
     """Vérifie si un mouvement en bas est possible
@@ -189,16 +150,10 @@ def checkDownMove(mazeDataF2, xF2, yF2):
         mazeDataF1 : tableau de tableau de cases | labyrinthe
 
     OUT
-        res : boolean
+        boolean
     """
-    res = True
-    if yF2 < len(mazeDataF2[0]): 
-        if mazeDataF2[xF2][yF2+1]["set"]: #Si la case du bas est définie, il y a un mouvement de moins possible
-            res = False
-    else: #Si la case est tout en bas, il y a un mouvement de moins possible
-        res = False
 
-    return res
+    return yF2 < len(mazeDataF2[0])-1 and mazeDataF2[xF2][yF2+1]["set"] == False
     
             
 def randomizeList(liste):
@@ -216,3 +171,25 @@ def randomizeList(liste):
         swap(i, random.randint(i+1, len(liste)-1))
 
     return res
+
+def drawMaze(mazeF3):
+    for j in range(0, len(mazeF3[0])):
+        ligne = ""
+        for i in range(0, len(mazeF3)):
+            if mazeF3[i][j]["wallLeft"]:
+                ligne=ligne+"|"
+            else:
+                ligne=ligne+" "
+
+            if mazeF3[i][j]["wallUp"]:
+                ligne=ligne+"̿"
+            else:
+                ligne=ligne+" "
+
+            if i == len(mazeF3) -1:
+                ligne = ligne + "|"
+        print(ligne)
+    ligne = " " + ("̿ "*(len(mazeF3[0])))
+    print(ligne)
+            
+            
