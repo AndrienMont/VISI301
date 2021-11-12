5366e981ba051ffd68d82abb4b2aeca53f54bf41
ThxZ3US <baptistegriva@gmail.com>
2021-11-12 16:48:13 +0100

    Affichage des cases du labyrinthe seulement lorsque le joueur se trouve dessus

--- dessinLaby.py
+++ dessinLaby.py
@@ -1,6 +1,6 @@
 import pygame
 
-def dessineDedale (origine_xF1, origine_yF1, tailleCaseF1, mazeF1, longF1, largF1, screenF1):
+def dessineDedale (origine_xF1, origine_yF1, tailleCaseF1, mazeF1, longF1, largF1, screenF1,posX,posY):
     """Dessine un labyrinthe dans une fenêtre pygame
 
         IN
@@ -15,8 +15,8 @@ def dessineDedale (origine_xF1, origine_yF1, tailleCaseF1, mazeF1, longF1, largF
         OUT
             Lignes sur la fenêtre pygame
         """
-    for i in range (len(mazeF1[0])):
-        for j in range (len(mazeF1)):
+    for i in range(len(mazeF1[0])):
+        for j in range(len(mazeF1)):
             if mazeF1[i][j]["wallLeft"]:
                 
                 Start_left = (origine_xF1+i*tailleCaseF1, origine_yF1+(j*tailleCaseF1)) 
@@ -32,3 +32,51 @@ def dessineDedale (origine_xF1, origine_yF1, tailleCaseF1, mazeF1, longF1, largF
     End_vertic_horiz = (origine_xF1+longF1 * tailleCaseF1,origine_yF1+ largF1 * tailleCaseF1)
     pygame.draw.line(screenF1,(166,15,64),End_vertical, End_vertic_horiz,2)
     pygame.draw.line(screenF1,(166,15,64), End_horizontal,End_vertic_horiz,2)
+
+
+
+
+
+
+
+def affiche_case(origine_xF2,origine_yF2,tailleCaseF2,mazeF2,longF2,largF2,screenF2,posX,posY):
+
+    #meme principe que la fonction dessinDedale mais affiche seulement une case
+
+    
+    if mazeF2[posX][posY]["wallLeft"]: #Affiche le mur de gauche
+        
+        Start_left = (origine_xF2+posX*tailleCaseF2, origine_yF2+(posY*tailleCaseF2)) 
+        End_left = (origine_xF2+posX*tailleCaseF2, origine_yF2+(posY*tailleCaseF2)+tailleCaseF2)
+        pygame.draw.line(screenF2, (166,15,64), Start_left, End_left, 2)
+
+
+    if mazeF2[posX][posY]["wallUp"]: #Affiche le mur du dessus
+        
+        Start_up = (origine_xF2+(posX*tailleCaseF2),origine_yF2+posY*tailleCaseF2)
+        End_up = (origine_xF2+(posX*tailleCaseF2)+tailleCaseF2,origine_yF2+posY*tailleCaseF2)
+        pygame.draw.line(screenF2, (166,15,64), Start_up, End_up,2)
+        
+
+    if posY+1 < len(mazeF2):
+        if mazeF2[posX][posY+1]["wallUp"]: #Affiche le mur du dessous
+        
+            Start_up = (origine_xF2+(posX*tailleCaseF2),origine_yF2+(posY*tailleCaseF2)+tailleCaseF2)
+            End_up = (origine_xF2+(posX*tailleCaseF2)+tailleCaseF2,origine_yF2+(posY*tailleCaseF2)+tailleCaseF2)
+            pygame.draw.line(screenF2, (166,15,64), Start_up, End_up,2)
+
+    if posX+1 < len(mazeF2):
+        if mazeF2[posX+1][posY]["wallLeft"]: #Affiche le mur de droite
+        
+            Start_left = (origine_xF2+(posX*tailleCaseF2)+tailleCaseF2, origine_yF2+(posY*tailleCaseF2)) 
+            End_left = (origine_xF2+(posX*tailleCaseF2)+tailleCaseF2, origine_yF2+(posY*tailleCaseF2)+tailleCaseF2)
+            pygame.draw.line(screenF2, (166,15,64), Start_left, End_left, 2)
+
+        
+            
+    End_vertical = (origine_xF2,origine_yF2+ largF2 * tailleCaseF2)
+    End_horizontal = (origine_xF2+longF2 * tailleCaseF2, origine_yF2)
+    End_vertic_horiz = (origine_xF2+longF2 * tailleCaseF2,origine_yF2+ largF2 * tailleCaseF2)
+    pygame.draw.line(screenF2,(166,15,64),End_vertical, End_vertic_horiz,2)
+    pygame.draw.line(screenF2,(166,15,64), End_horizontal,End_vertic_horiz,2)
+
--- prototype alpha-1.py
+++ prototype alpha-1.py
@@ -3,12 +3,10 @@ import sys
 import labyrinthe as laby
 import dessinLaby as dessLab
 from pygame.locals import *
-from labyrinthe import *
-from dessinLaby import*
-import random
+from random import*
 
 
-
+tailleCase = 20
 vel = 1
 
 class perso:
@@ -17,11 +15,12 @@ class perso:
         def __init__(self): # Chargement du sprite et definition de sa position
          
             self.personnage = pygame.image.load('perso.png').convert_alpha()
-            self.personnage = pygame.transform.scale(self.personnage , (20, 20))
+            self.personnage = pygame.transform.scale(self.personnage , (tailleCase, tailleCase))
             self.X = 0
             self.Y = 0
          
-        def update (self):                      # Ajout d'une fonction Update
+
+        def update (self):                      
          
             self.position = (self.X,self.Y)
  
@@ -40,16 +39,16 @@ class perso:
                 if direction == "bas":
                     self.direct = self.personnage
                     self.Y += vel
+                
 
 
 
-        
 
 def main():
 
     pygame.init()
-    # Effectuer un deplacement plusieurs fois en laissant appuyé sur la touche
     pygame.key.set_repeat(400, 100)
+    
     screen = pygame.display.set_mode((700,700))
     pygame.display.set_caption("Prototype Alpha-1 du projet MazeRunner")
     joueur = perso()
@@ -58,14 +57,14 @@ def main():
 
     origine_x = 0
     origine_y = 0
-    tailleCase = 20
+    posXmax = 0
+    posYmax = 0
     fin = pygame.transform.scale(fin , (tailleCase,tailleCase))
+    taille = 10
+    caseFin = tailleCase*taille-tailleCase
     couleur = (0,0,0)
-    difficulte = 10
-    dedale = laby.generateMaze(difficulte,difficulte)
-    laby.drawMaze(dedale)
-
-    dessLab.dessineDedale(origine_x,origine_y,tailleCase,dedale, 10,10,screen)
+    dedale = laby.generateMaze(taille,taille)
+    laby.drawMaze(dedale) 
 
     
     loop = True
@@ -73,47 +72,53 @@ def main():
         for event in pygame.event.get():
                 if event.type == QUIT:
                     loop = False
-                if joueur.X == 180 and joueur.Y == 180:
-                    loop = False
-                    
+
                 if event.type == KEYDOWN:
                     if event.key == K_LEFT:
-                        if yAMur(dedale,joueur.X,joueur.Y,"left"):
+                        if laby.yAMur(dedale,joueur.X,joueur.Y,"left"):
                                 print("collision")
                         else:
                                 joueur.bouge("gauche")
                     
-                if event.type == KEYDOWN:
                     if event.key == K_RIGHT:
-                        if yAMur(dedale,joueur.X,joueur.Y,"right"):
+                        if laby.yAMur(dedale,joueur.X,joueur.Y,"right"):
                                 print("collision")
                         else:
                                 joueur.bouge("droite")
                     
-                if event.type == KEYDOWN:
                     if event.key == K_UP:
-                        if yAMur(dedale,joueur.X,joueur.Y,"up"):
+                        if laby.yAMur(dedale,joueur.X,joueur.Y,"up"):
                                 print("collision")
                         else:
                                 joueur.bouge("haut")
                     
-                if event.type == KEYDOWN:
                     if event.key == K_DOWN:
-                        if yAMur(dedale,joueur.X,joueur.Y,"down"):
+                        if laby.yAMur(dedale,joueur.X,joueur.Y,"down"):
                                 print("collision")
                         else:
                                 joueur.bouge("bas")
+
+                if joueur.X > posXmax:
+                        posXmax = joueur.X
+
+                if joueur.Y > posYmax:
+                        posYmax = joueur.Y
+                        
+                if joueur.X*tailleCase == caseFin and joueur.Y*tailleCase == caseFin:
+                        loop = False
                 
+
+#Affichage
                         
         screen.fill(couleur)
-        dessLab.dessineDedale(origine_x,origine_y,tailleCase,dedale, 10,10,screen)
-        screen.blit(fin , ((tailleCase*difficulte-tailleCase),(tailleCase*difficulte-tailleCase)))
+        screen.blit(fin , (caseFin,caseFin))
+        dessLab.affiche_case(origine_x,origine_y,tailleCase,dedale, 10,10,screen,joueur.X,joueur.Y)
         screen.blit(joueur.personnage , (joueur.X*tailleCase, joueur.Y*tailleCase))
-        pygame.display.update()             
-                    
+        pygame.display.update()
+        
     pygame.quit()
 
-
+        
 
 
 main()
